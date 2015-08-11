b53f008a5bc46a5e9f03b5d00affba899159b1b7
bgamwell <bgamwell@gmail.com>
2015-08-10 20:53:09 -0700

    Fixing image upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def update
     user = User.find(session[:user_id])
     if current_user = user
-      form_params = params.require(:user).permit(:first_name, :last_name, :current_city)
+      form_params = params.require(:user).permit(:first_name, :last_name, :profile_image, :current_city)
       user.update_attributes(form_params)
       redirect_to profile_path
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
 	# Paperclip
 	has_attached_file :profile_image, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :profile_image, :content_type => /\Aimage\/.*\Z/
+
 	validates_with AttachmentSizeValidator, :attributes => :profile_image, :less_than => 1.megabytes
+
 end
