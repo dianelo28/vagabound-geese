7bd3df3e791e34d63c63b12a56ca8a827a0b755e
bgamwell <bgamwell@gmail.com>
2015-08-12 11:53:09 -0700

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_many :posts, dependent: :destroy
 
+	validates :username, uniqueness: true
+	validates :email, :password, presence: true
+  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+
+
 
 	# Paperclip
 	has_attached_file :profile_image,
