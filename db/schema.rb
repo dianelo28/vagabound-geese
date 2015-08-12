c9cea63cb8f184b012dbe5bb5626238ec1030833
Diane Lo <Diane@Dianes-MacBook-Pro.local>
2015-08-12 09:57:06 -0700

    style changes forms work

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150811184946) do
-=======
-ActiveRecord::Schema.define(version: 20150811031258) do
->>>>>>> 4652e1bd271466bf563643ed5ed543a9dc67ba2c
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
