371df8ad32e7b3ee4008d63b48579d39cf621984
Diane Lo <Diane@Dianes-MacBook-Pro.local>
2015-08-13 13:54:57 -0700

    comments work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
    def current_user
        @current_user ||= User.find_by(id: session[:user_id])
    end
+   
    def authorize
        unless current_user
          redirect_to login_path
